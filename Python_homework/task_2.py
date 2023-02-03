2a0c3c51e916eeef1a87a530881baf644a57798b
Max <makism.mush21@gmail.com>
2023-02-03 13:43:20 +0300

    fix

--- Python_homework/task_2.py
+++ Python_homework/task_2.py
@@ -1,4 +1,6 @@
 total = int(input('Всего журавликов: '))
-katia = total / 2
-petia_and_sasha = katia / 2
-print(f'Всего журавликов {total} из них Катя сделала {katia}, а Петя и Саша сделали по {petia_and_sasha}')
+katia = 0
+p_and_s = total / 6
+katia = 2 * total / 3
+
+print(f'Всего журавликов {total} из них Катя сделала {katia}, а Петя и Саша сделали по {p_and_s}')
--- Python_homework/task_4.py
+++ Python_homework/task_4.py
@@ -1,10 +1,10 @@
-n=int(input())
-m=int(input())
-k=int(input())
+n = int(input())
+m = int(input())
+k = int(input())
 
-if n*m<k or k==n*m:
+if n * m < k or k == n * m:
     print ('Нет')
-elif k%m==0 or k%n==0:
+elif k % m == 0 or k % n == 0:
     print ('Да')
 else:
     print ('Нет')
