9ffb272772f5b637b366092e73b91335465e9d95
Max <makism.mush21@gmail.com>
2023-02-05 15:30:03 +0300

    fix

--- Python_homework/task_1.py
+++
@@ -1,9 +0,0 @@
-number = input('Введите трёхзначное число: ')
-sum = 0
-for i in number:
-    if i.isdigit():
-        sum += int(i)
-print(sum)
-
-
-
--- Python_homework/task_2.py
+++
@@ -1,6 +0,0 @@
-total = int(input('Всего журавликов: '))
-katia = 0
-p_and_s = total / 6
-katia = 2 * total / 3
-
-print(f'Всего журавликов {total} из них Катя сделала {katia}, а Петя и Саша сделали по {p_and_s}')
